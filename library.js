3230e88fa3901dca1b4306b65270cdba51fa9e54
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2021-10-04 22:43:59 -0400

    Add function to add cards and populate with book info, and add checkbox for book completion status

--- library.js
+++ library.js
@@ -1,6 +1,10 @@
-// Simple Library App
+// Simple library app
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
 let library = [];
+const libContainer = $("#library");
 
 function Book(id, title, author=null, pageCount=null, isRead=false, series=null) {
     this.id = id;
@@ -16,6 +20,56 @@ Book.prototype.info = function() {
 
 function addBookToLibrary(book) {
     library.push(book);
+    const card = document.createElement("section"); 
+    card.classList.toggle("book-card");
+
+    // Associate card element with 
+    card.dataset.id = book.id;
+
+    const title = document.createElement("div"); 
+    title.classList.toggle("title");
+    title.textContent = book.title;
+    card.appendChild(title);
+
+    const author = document.createElement("div"); 
+    author.classList.toggle("author");
+    author.textContent = `by ${book.author}`;
+    card.appendChild(author);
+
+    const series = document.createElement("div"); 
+    series.classList.toggle("series");
+    series.textContent = book.series;
+    card.appendChild(series);
+
+    const pages = document.createElement("div"); 
+    pages.classList.toggle("pages");
+    pages.textContent = `${book.pageCount} pages`;
+    card.appendChild(pages);
+
+    const readStatus = document.createElement("div");
+    readStatus.classList.toggle("read-status");
+    card.appendChild(readStatus);
+
+    const readStatusToggle = document.createElement("input");
+    readStatusToggle.setAttribute("type", "checkbox");
+    readStatusToggle.classList.toggle("read-status-toggle");
+    readStatus.appendChild(readStatusToggle);
+    readStatusToggle.checked = book.isRead; 
+    readStatusToggle.addEventListener("change", function () {
+        book.isRead = !book.isRead;
+        readStatusLabel.textContent = `${book.isRead ? "Completed" : "Not completed"}`;
+    });
+
+    const readStatusLabel = document.createElement("label"); 
+    readStatusLabel.setAttribute("for", `book${book.id}`);
+    readStatusLabel.classList.toggle("read-status-label");
+    readStatusLabel.textContent = `${book.isRead ? "Completed" : "Not completed"}`;
+    readStatus.appendChild(readStatusLabel);
+
+    const deleteBookBtn = document.createElement("button")
+    deleteBookBtn.classList.toggle("del-button");
+
+    libContainer.appendChild(card);
 }
 
 function logLibrary(lib) {
@@ -29,7 +83,14 @@ function logLibrary(lib) {
 const b1 = new Book(1, "The Hobbit", "J.R.R. Tolkien", 227, true);
 const b2 = new Book(2, "The Fellowship of the Ring", "J.R.R. Tolkien", 371, true);
 const b3 = new Book(3, "The Silmarillian", "J.R.R. Tolkien", 408, false);
+const b4 = new Book(3, "The Silmarillian", "J.R.R. Tolkien", 408, false);
+const b5 = new Book(3, "The Silmarillian", "J.R.R. Tolkien", 408, false);
+const b6 = new Book(3, "The Silmarillian", "J.R.R. Tolkien", 408, false);
 
 addBookToLibrary(b1);
 addBookToLibrary(b2);
 addBookToLibrary(b3);
+addBookToLibrary(b4);
+addBookToLibrary(b5);
+addBookToLibrary(b6);
+
